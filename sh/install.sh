43ac040ac2b51113c4b9b71fb4b1b650dae4fca2
Josué Telles Corrêa <josuejtc@hotmail.com>
2017-09-24 20:42:28 -0300

    install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -1,12 +1,12 @@
-#yum install
+#yum install ok
 yum install curl 
 
-# Instalando Node
+# Instalando Node ok
 curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 sudo yum -y install nodejs
 sudo yum install gcc-c++ make
 
-#Vue - cli
+#Vue - cli ok
 npm install -g vue-cli
 
 #Asp.net core ok
@@ -16,35 +16,47 @@ sudo yum update
 sudo yum install libunwind libicu
 sudo yum install dotnet-sdk-2.0.0
 
-
 #Sql Server
-https://packages.microsoft.com/config/rhel/7/mssql-server.repo > /etc/yum.repos.d/mssql-server.repo 
-yum install mssql-server 
-/opt/mssql/bin/sqlservr-setup 
-firewall-cmd --zone=public --add-port=1433/tcp --permanent
-firewall-cmd --reload 
-systemctl status mssql-server 
+sudo curl -o /etc/yum.repos.d/mssql-server.repo https://packages.microsoft.com/config/rhel/7/mssql-server.repo
+sudo yum update
+sudo yum install -y mssql-server
+sudo /opt/mssql/bin/mssql-conf setup
+systemctl status mssql-server
+sudo firewall-cmd --zone=public --add-port=1433/tcp --permanent
+sudo firewall-cmd --reload
 
-#GIT
+sudo curl -o /etc/yum.repos.d/msprod.repo https://packages.microsoft.com/config/rhel/7/prod.repo
+sudo yum update
+sudo yum remove unixODBC-utf16 unixODBC-utf16-devel
+sudo yum update
+sudo yum install -y mssql-tools unixODBC-devel
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc 
+
+#GIT ok
 sudo yum install git
 
-#Pastas Padrao
+#Pastas Padrao ok
 sudo mkdir /var/www
 cd /var/www
 sudo chmod 777 /var/www
 
-#Clone aplicacao de eventos
+#Clone aplicacao de eventos ok
 git clone https://github.com/josuecorrea/vuejseventos.git /var/www/eventosfront
 cd /var/www/eventosfront
 npm install
 
-#Clone api
+#Clone api ok
 git clone https://github.com/josuecorrea/webapieventos.git /var/www/eventosapi
 cd /var/www/eventosapi
 dotnet restore
 dotnet build
 
-#NGINX
+dotnet ef migrations add InitialCreate
+dotnet ef database update
+
+#NGINX ok
 sudo yum install epel-release
 sudo yum install nginx
 sudo systemctl start nginx
